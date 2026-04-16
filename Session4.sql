15e3d9a824db981accc6c02febb7769ef498ef57
ProdByMts <105318992+ProdByMts@users.noreply.github.com>
2026-04-16 07:20:27 -0400

    actividad session 4 1 y 2

--- Session4.sql
+++ Session4.sql
@@ -1,3 +1,5 @@
+--session 4 (1)--
+
 DECLARE
     v_valor NUMBER;
     v_bias NUMBER := 50;           
@@ -19,6 +21,29 @@ EXCEPTION
     
     WHEN NO_DATA_FOUND THEN
         DBMS_OUTPUT.PUT_LINE('Error: No se encontró el registro solicitado en la tabla.');
+        
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error inesperado: ' || SQLERRM);
+END;
+
+
+--Session 4 (2)--
+
+DECLARE
+    unique_violation EXCEPTION;
+    PRAGMA EXCEPTION_INIT(unique_violation, -8001); 
+BEGIN
+    INSERT INTO Clientes (ClienteID, Nombre, Ciudad)
+    VALUES (1, 'Félix Nilo', 'Coquimbo'); 
+
+    DBMS_OUTPUT.PUT_LINE('Tupla insertada correctamente.');
+
+EXCEPTION
+    WHEN unique_violation THEN
+        DBMS_OUTPUT.PUT_LINE('Error TimesTen capturado: Violación de clave única. El ID ingresado ya existe (TT8001).');
+        
+    WHEN DUP_VAL_ON_INDEX THEN
+        DBMS_OUTPUT.PUT_LINE('Error Oracle capturado: Violación de restricción de unicidad (ORA-00001).');
         
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Error inesperado: ' || SQLERRM);
